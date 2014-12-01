19fb9fc04fbba18c2931c7b02f5c81def1aa0466
Administrador <ramon.calle.88@gmail.com>
2014-11-30 23:29:57 -0430

    Eliminado root.

--- src/MVC/View.php
+++ src/MVC/View.php
@@ -10,39 +10,37 @@ use \MVC\Errors\RuntimeException;
  * @author Ramón Serrano <ramon.calle.88@gmail.com>
  * @package MVC
  */
-class View {
+class View
+{
 
-    /**
-     * App root
-     * @var string $root 
-     */
-    public $root;
     /**
      * Path folder templates
      * @var string $templates_path
      */
-    public $templates_path;
+    public $templatesPath;
 
     /**
      *  Display the content of template
      *
      *  @param string $file    The file to be rendered.
      *  @param mixed $vars     The variables to be substituted in the view.
-     *  @access public
+     * 
      *  @return void
      */
-    public function display($file, $vars = null) {
-        echo $this->render($file, $vars);
+    public function display($file, $vars = null)
+    {
+        die($this->render($file, $vars));
     }
 
     /**
      *  Escapes a value for output in an HTML context.
      *
      *  @param mixed $value
-     *  @access public
-     *  @return mixed
+     * 
+     *  @return string
      */
-    public function escape($value) {
+    public function escape($value)
+    {
         return nl2br(htmlspecialchars($value, ENT_QUOTES, "UTF-8"));
     }
 
@@ -54,8 +52,9 @@ class View {
      *  @access public
      *  @return string
      */
-    public function render($file, $vars = null) {
-        $__template__ = "$this->root/$this->templates_path/{$file}";
+    public function render($file, $vars = null) 
+    {
+        $__template__ = "$this->templatesPath/{$file}";
         
         if(!file_exists($__template__)){
            RuntimeException::run("View '$__template__' don´t exists.");
