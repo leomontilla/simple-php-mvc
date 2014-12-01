edceca836e83d1459be786d38a1153c9fb5ef055
Administrador <ramon.calle.88@gmail.com>
2014-11-30 21:31:04 -0430

    Cambios de Prueba de Modelo.

--- tests/MVC/Tests/ModelTest.php
+++ tests/MVC/Tests/ModelTest.php
@@ -8,7 +8,8 @@
 
 namespace MVC\Tests;
 
-use MVC\DataBase\PDO;
+use MVC\DataBase\PDO,
+    MVC\Tests\MVC\ExampleModel;
 
 class ModelTest extends \PHPUnit_Framework_TestCase
 {
@@ -20,7 +21,7 @@ class ModelTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->_pdo = new PDO('mysql:host=localhost;dbname=MPPD_DB;charset=UTF8', 'root', '');
-        $this->_model = new Model($this->_pdo);
+        $this->_model = new ExampleModel($this->_pdo);
     }
     
     public function testQuery()
@@ -32,32 +33,82 @@ class ModelTest extends \PHPUnit_Framework_TestCase
     
     public function testInsert()
     {
-        $sql = 'INSERT INTO estados (id_estados, nombre) VALUES (?, ?)';
-        $params = array(25, 'Agregado');
+        $data = array('id_estados' => 25, 'nombre' => 'Agregado');
         
-        $filasAfectadas = $this->_model->insert($sql, $params);
+        $filasAfectadas = $this->_model->insert($data);
         
         $this->assertInternalType('int', $filasAfectadas);
     }
     
     public function testUpdate()
     {
-        $sql = 'UPDATE estados SET nombre = ? WHERE id_estados = ?';
-        $params = array('Cambiado', 25);
+        $data = array('nombre' => 'Agregado');
+        $criteria = array('id_estados' => 25);
         
-        $filasAfectadas = $this->_model->update($sql, $params);
+        $filasAfectadas = $this->_model->update($data, $criteria);
         
         $this->assertInternalType('int', $filasAfectadas);
     }
     
     public function testDelete()
     {
-        $sql = 'DELETE FROM estados WHERE id_estados = ?';
-        $params = array(25);
+        $criteria = array('id_estados' => 25);
         
-        $filasAfectadas = $this->_model->delete($sql, $params);
+        $filasAfectadas = $this->_model->delete($criteria);
         
         $this->assertInternalType('int', $filasAfectadas);
     }
+    
+    public function testSelectAll()
+    {
+        $fields = array('*');
+        $criteria = array();
+        $operators = array('=');
+        $conditions = array('AND');
+        
+        $estados = $this->_model->select($fields, $criteria, $operators, $conditions);
+        
+        $this->assertInternalType('array', $estados);
+    }
+    
+    public function testSelectEspecific()
+    {
+        $fields = array('id_estados', 'nombre');
+        $criteria = array('id_estados' => 24);
+        $operators = array('=');
+        
+        $estados = $this->_model->select($fields, $criteria, $operators);
+        
+        $this->assertInternalType('array', $estados);
+    }
+    
+    public function testSelectAndOrCondition()
+    {
+        $fields = array('id_estados', 'nombre');
+        $criteria = array('id_estados' => 2, 'nombre' => 'Aragua');
+        $operators = array('!=', '!=');
+        $conditions = array("AND");
+        $operators2 = array('=', '=');
+        $conditions2 = array('OR');
+        
+        $estados = $this->_model->select($fields, $criteria, $operators, $conditions);
+        $estados2 = $this->_model->select($fields, $criteria, $operators2, $conditions2);
+        
+        $this->assertInternalType('array', $estados);
+        $this->assertInternalType('array', $estados2);
+    }
 
+    public function testFindAll()
+    {
+        $estados = $this->_model->findAll();
+        
+        $this->assertInternalType('array', $estados);
+    }
+    
+    public function testFindBy()
+    {
+        $estados = $this->_model->findBy('nombre', 'Aragua');
+        
+        $this->assertInternalType('array', $estados);
+    }
 }
