f9061dfe9d7f8c55071bebfdfa4cd4c52e5c0714
Ramón Serrano <ramon.calle.88@gmail.com>
2015-01-08 02:24:56 -0430

    agregado use.

--- src/MVC/Console/Output/StreamOutput.php
+++ src/MVC/Console/Output/StreamOutput.php
@@ -1,12 +1,8 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace MVC\Console\Output;
+
+use MVC\Console\Formatter\OutputFormatterInterface;
 
 /**
  * Description of StreamOutput
