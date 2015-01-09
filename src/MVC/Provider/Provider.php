321adc6d8f0e7c20ed5990bb0e3912c68c824327
Administrador <ramon.calle.88@gmail.com>
2015-01-08 22:03:28 -0430

    Agregada funcion getName().

--- src/MVC/Provider/Provider.php
+++ src/MVC/Provider/Provider.php
@@ -9,5 +9,27 @@ namespace MVC\Provider;
  */
 abstract class Provider implements ProviderInterface
 {
+    /**
+     * The provider name
+     * 
+     * @var string
+     */
+    protected $name;
+    
+    /**
+     * Returns the provider name (the class short name).
+     *
+     * @return string The Module name
+     */
+    final public function getName()
+    {
+        if (null !== $this->name) {
+            return $this->name;
+        }
 
+        $name = get_class($this);
+        $pos = strrpos($name, '\\');
+
+        return $this->name = false === $pos ? $name : substr($name, $pos + 1);
+    }
 }
