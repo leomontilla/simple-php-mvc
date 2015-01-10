aca46cfd5727594d10f76e5597ee844ad5642422
Administrador <ramon.calle.88@gmail.com>
2015-01-10 00:57:12 -0430

    Cambiado Exception por LogicException y annotations.

--- src/MVC/View.php
+++ src/MVC/View.php
@@ -1,14 +1,13 @@
 <?php
 
+namespace MVC;
+
 /**
  * View
  * 
  * @author Ramón Serrano <ramon.calle.88@gmail.com>
  * @package MVC
  */
-
-namespace MVC;
-
 class View
 {
 
@@ -21,6 +20,7 @@ class View
 
     /**
      * Display the content of template
+     * 
      * @access public
      * @param string $file    The file to be rendered.
      * @param mixed $vars     The variables to be substituted in the view.
@@ -33,6 +33,7 @@ class View
 
     /**
      * Escapes a value for output in an HTML context.
+     * 
      * @access public
      * @param mixed $value
      * @return string
@@ -44,17 +45,19 @@ class View
 
     /**
      * Renders a given file with the supplied variables.
+     * 
      * @access public
      * @param string $file    The file to be rendered.
      * @param mixed $vars     The variables to be substituted in the view.
      * @return string
+     * @throws \LogicException
      */
     public function render($file, $vars = null) 
     {
         $__template__ = "$this->templatesPath/{$file}";
         
         if(!file_exists($__template__)){
-           throw new \Exception("View '$__template__' don´t exists.");
+           throw new \LogicException(sprintf('View "%s" don\'t exists.', $__template__));
         }
         
         if (is_array($vars)) {
