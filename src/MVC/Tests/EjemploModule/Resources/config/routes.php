1080e8359890d5d4c6f91a7747817cd5ead6d5b9
Administrador <ramon.calle.88@gmail.com>
2015-01-10 02:02:51 -0430

    Actualizadas las rutas.

--- src/MVC/Tests/EjemploModule/Resources/config/routes.php
+++ src/MVC/Tests/EjemploModule/Resources/config/routes.php
@@ -2,9 +2,21 @@
 
 return array(
     array(
-        "method" => ["GET", "POST"],
-        "pattern" => "/ejemplo/index",
-        "action" => "EjemploModule\\Controller\\DefaultController::ejemploPhpAction",
-        "name" => "ejemplo_index"
+        "method"  => ["GET", "POST"],
+        "pattern" => "/foo",
+        "action"  => "MVC\\Tests\\EjemploModule\\Controller\\FooController::indexAction",
+        "name"    => "foo_index"
+    ),
+    array(
+        "method"  => ["GET", "POST"],
+        "pattern" => "/foo/redirect",
+        "action"  => "MVC\\Tests\\EjemploModule\\Controller\\FooController::redirectAction",
+        "name"    => "foo_redirect"
+    ),
+    array(
+        "method"  => ["GET", "POST"],
+        "pattern" => "/foo/redirected",
+        "action"  => "MVC\\Tests\\EjemploModule\\Controller\\FooController::redirectedAction",
+        "name"    => "foo_redirected"
     )
 );
