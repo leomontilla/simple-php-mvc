9af0ce24c464ea72128d365704f7cd4521030962
Administrador <ramon.calle.88@gmail.com>
2015-01-25 14:48:23 -0430

    Actualizadas las rutas de TestModule.

--- src/MVC/Tests/TestModule/Resources/config/routes.php
+++ src/MVC/Tests/TestModule/Resources/config/routes.php
@@ -4,19 +4,19 @@ return array(
     array(
         "method"  => ["GET", "POST"],
         "pattern" => "/foo",
-        "action"  => "MVC\\Tests\\EjemploModule\\Controller\\FooController::indexAction",
+        "action"  => "MVC\\Tests\\TestModule\\Controller\\FooController::indexAction",
         "name"    => "foo_index"
     ),
     array(
         "method"  => ["GET", "POST"],
         "pattern" => "/foo/redirect",
-        "action"  => "MVC\\Tests\\EjemploModule\\Controller\\FooController::redirectAction",
+        "action"  => "MVC\\Tests\\TestModule\\Controller\\FooController::redirectAction",
         "name"    => "foo_redirect"
     ),
     array(
         "method"  => ["GET", "POST"],
         "pattern" => "/foo/redirected",
-        "action"  => "MVC\\Tests\\EjemploModule\\Controller\\FooController::redirectedAction",
+        "action"  => "MVC\\Tests\\TestModule\\Controller\\FooController::redirectedAction",
         "name"    => "foo_redirected"
     )
 );
